f94de6a7a0fb4a5458fd06614182131bec7d9f16
uselessconsumer <eliottdale@gmail.com>
2023-12-06 19:45:45 -0500

    create a function to switch the font to sans serif

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const intChar = document.querySelectorAll('.interactiveChar');
+
+
+
+intChar.forEach((span = document.querySelectorAll('.interactiveChar')) => {
+    var spanText = span.textContent;
+    console.log(spanText);
+    
+    span.addEventListener('click', () => {
+        console.log(spanText);
+        span.style.fontFamily = 'sans-serif';
+    });
+});
+
