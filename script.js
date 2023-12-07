35a0620497356c20090b1057b77de7ad70e31bbf
uselessconsumer <eliottdale@gmail.com>
2023-12-06 20:35:39 -0500

    modify funciton for mouseon event instead of click

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const intChar = document.querySelectorAll('.interactiveChar');
 
 
-
 intChar.forEach((span = document.querySelectorAll('.interactiveChar')) => {
     var spanText = span.textContent;
     console.log(spanText);
     
-    span.addEventListener('click', () => {
-        console.log(spanText);
+    span.addEventListener('mouseover', () => {
         span.style.fontFamily = 'sans-serif';
     });
+
 });
 
