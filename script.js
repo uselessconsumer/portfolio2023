b1d1896d5c6687c79fb2711e3b98c13c5c4ba58f
uselessconsumer <eliottdale@gmail.com>
2023-12-11 16:50:15 -0500

    change the if loop to an array

--- script.js
+++ script.js
@@ -1,48 +1,89 @@
+//maybe change the if else font hovering into an object?
+
+
 const intChar = document.querySelectorAll('.interactiveChar');
 
+let charTimer;
+
+//executes if a character hasn't been hovered in 10 seconds
+function timeDelay() {
+    console.log("time triggered");
+}
+
+
+//fontlist for interactive title
+let fonts = [
+    {
+        name: 'Grad',
+        fontFamily: 'grad, serif',
+        fontWeight: 700,
+    },
+    {
+        name: 'Lo-Res 21',
+        fontFamily: 'lores-21-serif, sans-serif',
+        fontWeight: 400,
+    },
+    {
+        name: 'Dunbar Text',
+        fontFamily: 'dunbar-text, sans-serif',
+        fontWeight: 800,
+    },
+    {
+        name: 'Bely Display',
+        fontFamily: 'bely-display, sans-serif',
+        fontWeight: 400,
+    },
+    {
+        name: 'ITC Avant Garde Gothic Pro',
+        fontFamily: 'itc-avant-garde-gothic-pro, sans-serif',
+        fontWeight: 700,
+    },
+    {
+        name: 'Swear Display',
+        fontFamily: 'swear-display, serif',
+        fontWeight: 700,
+    },
+    {
+        name: 'LTR Slab Extended Variable',
+        fontFamily: 'ltr-slab-extended-variable, sans-serif',
+        fontWeight: 700,
+    },
+    {
+        name: 'Megascope Variable',
+        fontFamily: 'megascope-variable, sans-serif',
+        fontWeight: 700,
+    },
+    {
+        name: 'Kit Rounded',
+        fontFamily: 'kit-rounded, sans-serif',
+        fontWeight: 800,
+    },
+]
+
+console.log(fonts[2].fontFamily);
+
+console.log(fonts);
 
 //when the mouse hovers over a new character
 intChar.forEach((span) => {
     
     span.addEventListener('mouseover', () => {
-        span.textContent.toLowerCase();
 
-        let randNum = Math.floor(Math.random() * 9);
+        clearTimeout(charTimer);
+        charTimer = setTimeout(timeDelay, 2000);
 
-        //if randNum === already used font run event again
-        //run a function that randomly changes the font weight as well(potential idea)
-        if (randNum === 0) {
-            span.style.fontFamily = 'grad, serif';
-            span.style.fontWeight = '700';
-        } else if (randNum === 1) {
-            span.style.fontFamily = 'lores-21-serif, sans-serif';
-            span.style.fontWeight = '400';
-        } else if (randNum === 2) {
-            span.style.fontFamily = 'dunbar-text, sans-serif';
-            span.style.fontWeight = '800';
-        } else if (randNum === 3) {
-            span.style.fontFamily = 'bely-display, sans-serif';
-            span.style.fontWeight = '400';
-        } else if (randNum === 4) {
-            span.style.fontFamily = 'itc-avant-garde-gothic-pro, sans-serif';
-            span.style.fontWeight = '700';
-        } else if (randNum === 5) {
-            span.style.fontFamily = 'swear-display, serif';
-            span.style.fontWeight = '700';
-        } else if (randNum === 6) {
-            span.style.fontFamily = 'ltr-slab-extended-variable, sans-serif';
-            span.style.fontWeight = '700';
-        } else if (randNum === 7) {
-            span.style.fontFamily = 'megascope-variable, sans-serif';
-            span.style.fontWeight = '700';
-        } else if (randNum === 8) {
-            span.style.fontFamily = 'kit-rounded, sans-serif';
-            span.style.fontWeight = '800';
-        }
+        //generates a random number in the font array to change the character that is hovered
+        let randNum = Math.floor(Math.random() * fonts.length);
+        span.style.fontFamily = fonts[randNum].fontFamily;
 
-
-
-        
     });
 });
+
+
+//a char is hovered
+//a timer is started
+//if another character isn't hovered within 10 seconds
+//change a random character
+//don't change the same character twice in a row
+//continue the loop until a character is hovered
 
