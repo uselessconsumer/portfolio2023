f37090837b3260a2eb96bd70c2494e964a86cca5
uselessconsumer <eliottdale@gmail.com>
2023-12-11 13:43:48 -0500

    update text hover effect to include more fonts

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const intChar = document.querySelectorAll('.interactiveChar');
 
 
-intChar.forEach((span = document.querySelectorAll('.interactiveChar')) => {
-    var spanText = span.textContent;
-    console.log(spanText);
+//when the mouse hovers over a new character
+intChar.forEach((span) => {
+    var spanText = intChar.textContent;
     
     span.addEventListener('mouseover', () => {
-        span.style.fontFamily = 'sans-serif';
-    });
 
+        let randNum = Math.floor(Math.random() * 7);
+
+
+        //if randNum === already used font run event again
+        //run a function that randomly changes the font weight as well(potential idea)
+        if (randNum === 0) {
+            span.style.fontFamily = 'itc-avant-garde-gothic-pro, sans-serif';
+            span.style.fontWeight = '500';
+        } else if (randNum === 1) {
+            span.style.fontFamily = 'swear-display, serif';
+            span.style.fontWeight = '400';
+        } else if (randNum === 2) {
+            span.style.fontFamily = 'megascope-variable, sans-serif';
+            span.style.fontWeight = '700';
+        } else if (randNum === 3) {
+            span.style.fontFamily = 'ltr-slab-extended-variable, sans-serif';
+            span.style.fontWeight = '300';
+        } else if (randNum === 4) {
+            span.style.fontFamily = 'grad, serif';
+            span.style.fontWeight = '400';
+        } else if (randNum === 5) {
+            span.style.fontFamily = 'dilemma-variable, sans-serif';
+            span.style.fontWeight = '400';
+        } else if (randNum === 6) {
+            span.style.fontFamily = 'bely-display, sans-serif';
+            span.style.fontWeight = '400';
+        }
+
+        
+    });
 });
 
